e4c1dcc4f3f409d0e431849b158be10043b53c6e
luckykushwaha249-hue <luckykushwaha249@gmail.com>
2026-07-20 01:14:13 +0530

    Create logger.py

---
+++ utils/utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+from datetime import datetime
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+
+def get_logger(name: str):
+    return logging.getLogger(name)
