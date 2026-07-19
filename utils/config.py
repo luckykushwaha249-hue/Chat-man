31ade85dfc95de40ae38d12c4379df4954347941
luckykushwaha249-hue <luckykushwaha249@gmail.com>
2026-07-20 01:13:25 +0530

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,17 @@
+class Config:
+    APP_NAME = "chat-man"
+    APP_PASSWORD = "8707352902"
+    BUFFER_SIZE = 4096
+    RFCOMM_CHANNEL = 4
+    HEARTBEAT_INTERVAL = 3  # seconds
+    TIMEOUT_LIMIT = 10  # 10 seconds pop-up rule
+    
+    # Theme Colors
+    BG_COLOR = "#0f172a"
+    PANEL_COLOR = "#1e293b"
+    ACCENT_COLOR = "#0ea5e9"
+    TEXT_COLOR = "#f8fafc"
+    
+    # Message Colors 
+    SENDER_BUBBLE = "#0ea5e9"
+    RECEIVER_BUBBLE = "#334155"
